812201e743c48f62ad01bf4b4e479c42495d9be4
Abhay Kumar poddar <57625588+Abhsonu143@users.noreply.github.com>
2022-07-21 00:40:08 +0530

    Update login.jsx

--- pages/user/login.jsx
+++ pages/user/login.jsx
@@ -15,7 +15,7 @@ const Login = () => {
     const handleClick=async()=>{
         var res;
         try{
-            res=await axios.post("http://localhost:3000/api/user",{username});
+            res=await axios.post("http://spirit-one.vercel.app/api/user",{username});
         }catch(err){
             console.log(err);
         }
