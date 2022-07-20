03297426acc4843da0b4654f51513c49f7d9a1f4
Abhay Kumar poddar <57625588+Abhsonu143@users.noreply.github.com>
2022-07-21 00:44:59 +0530

    Update [id].jsx

--- pages/orders/[id].jsx
+++ pages/orders/[id].jsx
@@ -91,7 +91,7 @@ const Order = ({order}) => {
   )
 }
 export const getServerSideProps=async ({params})=>{
-  const res=await axios.get(`http://localhost:3000/api/orders/${params.id}`);
+  const res=await axios.get(`https://spirit-one.vercel.app/api/orders/${params.id}`);
   return {
     props:{ order:res.data}
   };
