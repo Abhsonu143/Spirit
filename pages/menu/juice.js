49ef77b81b509063250f18634607a942983790e5
Abhay Kumar poddar <57625588+Abhsonu143@users.noreply.github.com>
2022-07-21 00:48:24 +0530

    Update juice.js

--- pages/menu/juice.js
+++ pages/menu/juice.js
@@ -16,7 +16,7 @@ export default function MenuList({menuItems}){
     )
 }
 export const getServerSideProps=async()=>{
-    const res=await axios.get("http://localhost:3000/api/products/juice");
+    const res=await axios.get("https://spirit-one.vercel.app/api/products/juice");
     return{
         props:{
             menuItems:res.data,
